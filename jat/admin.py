015ac61fa8d20181b73a1bef911045c625c12dc6
Seungmin Yi <s2019w33@e-mirim.hs.kr>
2021-05-26 10:14:18 +0900

    jat/admin Repository, Introduction, Comment

--- jat/admin.py
+++ jat/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from jat.models import Repository, Introduction, Comment
+
+admin.site.register(Repository)
+admin.site.register(Introduction)
+admin.site.register(Comment)
+
