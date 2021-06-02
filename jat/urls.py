d045558d27371502938f3cfa1238b6171f2c67a7
Seungmin Yi <s2019w33@e-mirim.hs.kr>
2021-06-02 10:02:53 +0900

    jat/urls repository_add

--- jat/urls.py
+++ jat/urls.py
@@ -7,4 +7,6 @@ app_name = 'jat'
 urlpatterns = [
     path('', views.RepositoryListView.as_view(), name='repository_list'),  #jat:repository_list
     path('repository/<int:pk>/', views.RepositoryDetailView.as_view(), name='repository_detail'),  #jat:repository_list
+    path('repository/<int:repository_pk>/introduction/<int:pk>/', views.IntroductionDetailView.as_view(), name='introduction_detail'),
+    path('repository/add/', views.RepositoryCreateView.as_view(), name='repository_add'),  # jat:repository_add
 ]
